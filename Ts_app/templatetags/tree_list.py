38bf0015acfa11ceb5703b56aef3f224e6d65f1c
zhangq <qi.zhang@brisky.cc>
2017-03-17 13:58:34 +0800

    a

--- Ts_app/templatetags/tree_list.py
+++ Ts_app/templatetags/tree_list.py
@@ -1,8 +1,47 @@
 from django import template
 register = template.Library()
-
+from django.utils.safestring import SafeData, mark_safe
 
 def li_tree(value):
-    return value.lower()
+    def walk_items(item_list):
+        item_iterator = iter(item_list)
+        try:
+            item = next(item_iterator)
+            
+            while True:
+                try:
+                    next_item = next(item_iterator)
+                except StopIteration:
+                    yield item, None
+                    break
+                if not isinstance(next_item, tuple):
+                    try:
+                        iter(next_item)
+                    except TypeError:
+                        pass
+                    else:
+                        yield item, next_item
+                        item = next(item_iterator)
+                        continue
+                yield item, None
+                item = next_item
+                
+        except StopIteration:
+            pass
+
+    def list_formatter(item_list, tabs=1):
+        indent = '\t' * tabs
+        output = []
+        for item, children in walk_items(item_list):
+            sublist = ''
+            if children:
+                sublist = '\n%s<ul>\n%s\n%s</ul>\n%s' % (
+                    indent, list_formatter(children, tabs + 1), indent, indent)
+                #print item
+                output.append('%s<li><font color="black"><a>%s</a>%s</li>' % (indent, item[0], sublist))
+            else:
+                output.append('%s<li><a href="/testcase/%s">%s</a>%s</li>' % (indent, item[1], item[0], sublist))    
+        return '\n'.join(output)
+    return mark_safe(list_formatter(value))
 
 register.filter("li_tree", li_tree)
